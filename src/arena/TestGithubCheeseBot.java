2d602feef8592adc590db69324800027a575daf3
AmmarFaisal <107199979+AmmarFaisal@users.noreply.github.com>
2022-12-13 11:00:41 -0500

    FINAL TEST AMMAR

--- src/arena/TestGithubCheeseBot.java
+++ src/arena/TestGithubCheeseBot.java
@@ -1,3 +1,4 @@
+
 package arena;
 import bots.GithubCheese;
 
@@ -6,7 +7,7 @@ import bots.GithubCheese;
  * @author Ammar Faisal
  * 
  * 
-* NOTE: ALL METHODS HAVE THEIR INPUTS SO OUTPUTS OF METHODS HAVE NOTHING TO DO WITH EACHOTHER AS PER 
+* NOTE: ALL METHODS HAVE THEIR OWN INPUTS SO OUTPUTS OF METHODS HAVE NOTHING TO DO WITH EACHOTHER AS PER 
 * THE REQUIREMENTS OF THE ASSIGNMENT.
  * 
  * 
@@ -23,91 +24,182 @@ public class TestGithubCheeseBot {
          */
         GithubCheese githubCheese = new GithubCheese();
 
-        /*x and y values of our bots location used for testing (so that they can be changed quickly) */
-        double x = 10;
-        double y = 10;
-
+  
         /*Test the getDistanceToEdge method. This method is expected to return the distance between a bot and an edge to 
-         * to ensure that our bot can perform one of its basic functions. Alter the X and Y values above to test different scenarios.
+         * to ensure that our bot can perform one of its basic functions. 
         */
+        
+        double x = 10;             
+        double y = 10;
+        System.out.println("THE BOT SHOULD PRINT THE DISTANCE FROM THE BOT TO THE EDGES");
         double distanceFromBottomEdge = githubCheese.getDistanceToEdge(BattleBotArena.BOTTOM_EDGE, x, y);
-        System.out.println("Distance from bottom edge: " + distanceFromBottomEdge);
+        System.out.println("Distance from bottom edge: " + distanceFromBottomEdge + ". TEST SUCCESSFUL.");
 
         double distanceFromTopEdge = githubCheese.getDistanceToEdge(BattleBotArena.TOP_EDGE, x, y);
-        System.out.println("Distance from top edge: " + distanceFromTopEdge);
+        System.out.println("Distance from top edge: " + distanceFromTopEdge + ". TEST SUCCESSFUL.");
 
         double distanceFromLeftEdge = githubCheese.getDistanceToEdge(BattleBotArena.LEFT_EDGE, x, y);
-        System.out.println("Distance from left edge: " + distanceFromLeftEdge);
+        System.out.println("Distance from left edge: " + distanceFromLeftEdge + ". TEST SUCCESSFUL.");
 
         double distanceFromRightEdge = githubCheese.getDistanceToEdge(BattleBotArena.RIGHT_EDGE, x, y);
-        System.out.println("Distance from right edge: " + distanceFromRightEdge);
+        System.out.println("Distance from right edge: " + distanceFromRightEdge + ". TEST SUCCESSFUL." + "\n");
+
+        
 
         /*Test the chooseEdge method. This method is expected to return the Chosen edge to the bot based on its x and y position.
         * This Method can be altered by changing X and Y values above to test different scenarios.
         */
-        int chosenEdge = githubCheese.chooseEdge(x, y);
+        double x1 = 20;
+        double y1 = 40;
+        
+        System.out.println("THE BOT SHOULD PRINT THE EDGE IT HAS CHOSEN WHICH IN THIS CASE IS THE LEFT EDGE");
+        int chosenEdge = githubCheese.chooseEdge(x1, y1);
         if (chosenEdge == BattleBotArena.BOTTOM_EDGE) 
-            System.out.println( "Chosen edge: Bottom"); 
+            System.out.println( "Chosen edge: Bottom\n"); 
             
         else if (chosenEdge == BattleBotArena.TOP_EDGE)
-            System.out.println( "Chosen edge: Top");
+            System.out.println( "Chosen edge: Top\n");
 
         else if (chosenEdge == BattleBotArena.LEFT_EDGE)
-            System.out.println( "Chosen edge: Left");
+            System.out.println( "Chosen edge: Left\n");
             
         else if (chosenEdge == BattleBotArena.RIGHT_EDGE)
-            System.out.println( "Chosen edge: Right");
+            System.out.println( "Chosen edge: Right\n");
+
+        x1 = 400;                  /*I Changed X and Y coordinates of the bot here to make sure the bot chooses the nearest edge based on the given situation   */
+        y1 = 400;
+        System.out.println("THE BOT SHOULD PRINT THE EDGE IT HAS CHOSEN WHICH IN THIS CASE IS THE RIGHT EDGE");
+        chosenEdge = githubCheese.chooseEdge(x1, y1);
+        if (chosenEdge == BattleBotArena.BOTTOM_EDGE) 
+            System.out.println( "Chosen edge: Bottom\n"); 
+            
+        else if (chosenEdge == BattleBotArena.TOP_EDGE)
+            System.out.println( "Chosen edge: Top\n");
+
+        else if (chosenEdge == BattleBotArena.LEFT_EDGE)
+            System.out.println( "Chosen edge: Left\n");
+            
+        else if (chosenEdge == BattleBotArena.RIGHT_EDGE)
+            System.out.println( "Chosen edge: Right\n");
+
+
+
+        
         
         /*Test the lastMoveWasFire. This method is expected to return true if the previous move was a firing move, and false if it was not. 
-         * int move = BattleBotArena.FIREDOWN;
-         * int move = BattleBotArena.FIRELEFT;
-         * int move = BattleBotArena.FIRERIGHT;
         */
-        int move = BattleBotArena.FIREUP; /*This value can be altered with above value to produce different scenarios */
+        int move = BattleBotArena.FIREUP; 
         boolean lastMoveWasFire = githubCheese.lastMoveWasFire(move);
         if (lastMoveWasFire == true)
-            System.out.println("Last move was firing move");
+            System.out.println("FIREUP test successful.");
         else
-            System.out.println("Last move was not a firing move");
+            System.out.println("FIREUP test failed.");
 
-        /*Test the moveToEdge method. This method is expected to return the correct move that the bot needs in order to move to its chosen edge.
-         * int choseEdge = BattleBotArena.Bottom_EDGE;
-         * int choseEdge = BattleBotArena.Left_EDGE;
-         * int choseEdge = BattleBotArena.Right_EDGE;
-         * use the above values to test the moveToEdge method
-         */
-        int choseEdge = BattleBotArena.BOTTOM_EDGE; /*This value can be altered with above value to produce different scenarios */
-        int move1 = githubCheese.moveToEdge(x, y, choseEdge); 
+        move = BattleBotArena.FIREDOWN;
+        lastMoveWasFire = githubCheese.lastMoveWasFire(move);
+        if (lastMoveWasFire == true)
+            System.out.println("FIREDOWN test successful.");
+        else
+            System.out.println("FIREDOWN test failed.");
+
+        move = BattleBotArena.FIRELEFT; 
+        lastMoveWasFire = githubCheese.lastMoveWasFire(move);
+        if (lastMoveWasFire == true)
+            System.out.println("FIRELEFT test successful.");
+        else
+            System.out.println("FIRELEFT test failed.");
+
+        move = BattleBotArena.FIRERIGHT; 
+        lastMoveWasFire = githubCheese.lastMoveWasFire(move);
+        if (lastMoveWasFire == true)
+            System.out.println("FIRERIGHT test successful.\n");
+        else
+            System.out.println("FIRERIGHT test failed.");
+
+
+
+        /*Test the moveToEdge method. This method is expected to return the correct move that the bot needs in order to move to its chosen edge. */
+        double x2 = 100;
+        double y2 = 100;
+
+        int choseEdge = BattleBotArena.BOTTOM_EDGE; 
+        int move1 = githubCheese.moveToEdge(x2, y2, choseEdge); 
+        if (move1 == BattleBotArena.DOWN)
+            System.out.println("MOVE to BOTTOM EDGE test successful.");
+        
+        else
+            System.out.println("Move to edge failed"); 
+
+            
+        choseEdge = BattleBotArena.TOP_EDGE;                      /*By changng the value of choseEdge, I can make sure that the bot makes the right decision to go to the chosen edge */
+        move1 = githubCheese.moveToEdge(x2, y2, choseEdge);
         if (move1 == BattleBotArena.UP)
-            System.out.println("Move to top edge");
-        else if (move1 == BattleBotArena.DOWN)
-            System.out.println("Move to bottom edge");
-        else if (move1 == BattleBotArena.LEFT)
-            System.out.println("Move to left edge");
-        else if (move1 == BattleBotArena.RIGHT)
-            System.out.println("Move to right edge");
+            System.out.println("MOVE to TOP EDGE test successful.");      
+        else
+            System.out.println("Move to edge failed");
+
+        
+        choseEdge = BattleBotArena.LEFT_EDGE;
+        move1 = githubCheese.moveToEdge(x2, y2, choseEdge);
+        if (move1 == BattleBotArena.LEFT)
+            System.out.println("MOVE to LEFT EDGE test successful.");
+        else
+            System.out.println("Move to edge failed");
+
+        
+        choseEdge = BattleBotArena.RIGHT_EDGE;
+        move1 = githubCheese.moveToEdge(x2, y2, choseEdge);
+        if (move1 == BattleBotArena.RIGHT)
+            System.out.println("MOVE to RIGHT EDGE test successful.\n");
         else
             System.out.println("Move to edge failed");
 
         /*Test the cycleOnEdge method. This method is expected to return the correct move that the bot needs in order to cycle on its edge. */
         int lastmove = BattleBotArena.UP;
-        int choseEdge2 = BattleBotArena.LEFT_EDGE; /*These values can be altered with above value to produce different scenarios */
+         
         boolean shotOk = true;
-        double pShoot = 0.3;  
+        double pShoot = 0;  
 
 
-
+        int choseEdge2 = BattleBotArena.LEFT_EDGE;
         int cycling = githubCheese.cycleOnEdge(x, y, lastmove, choseEdge2, shotOk, pShoot);
+        System.out.println("The bot is on left edge. It should move UP or DOWN.");
         if (cycling == BattleBotArena.UP)
-            System.out.println("Cycle on left edge");
+            System.out.println("BOT MOVED UP\n");
         else if (cycling == BattleBotArena.DOWN)
-            System.out.println("Cycle on left edge");
-        else if (cycling == BattleBotArena.LEFT)
-            System.out.println("Cycle on left edge");
-        else if (cycling == BattleBotArena.RIGHT)
-            System.out.println("Cycle on left edge");
+            System.out.println("BOT MOVED DOWN\n");
         else
-            System.out.println("Cycle on edge failed");
+            System.out.println("Cycle on edge failed\n");
+
+        choseEdge2 = BattleBotArena.RIGHT_EDGE;
+        cycling = githubCheese.cycleOnEdge(x, y, lastmove, choseEdge2, shotOk, pShoot);
+        System.out.println("The bot is on RIGHT edge. It should move UP or DOWN.");
+        if (cycling == BattleBotArena.UP)
+            System.out.println("BOT MOVED UP\n");
+        else if (cycling == BattleBotArena.DOWN)
+            System.out.println("BOT MOVED DOWN\n");
+        else
+            System.out.println("Cycle on edge failed\n");
+
+        chosenEdge = BattleBotArena.TOP_EDGE;
+        cycling = githubCheese.cycleOnEdge(x, y, lastmove, chosenEdge, shotOk, pShoot);
+        System.out.println("The bot is on TOP edge. It should move LEFT or RIGHT.");
+        if (cycling == BattleBotArena.LEFT)
+            System.out.println("BOT MOVED LEFT\n");
+        else if (cycling == BattleBotArena.RIGHT)
+            System.out.println("BOT MOVED RIGHT\n");
+        else
+            System.out.println("Cycle on edge failed\n");
+
+        chosenEdge = BattleBotArena.BOTTOM_EDGE;    
+        cycling = githubCheese.cycleOnEdge(x, y, lastmove, chosenEdge, shotOk, pShoot);
+        System.out.println("The bot is on BOTTOM edge. It should move LEFT or RIGHT.");
+        if (cycling == BattleBotArena.LEFT)
+            System.out.println("BOT MOVED LEFT\n");
+        else if (cycling == BattleBotArena.RIGHT)
+            System.out.println("BOT MOVED RIGHT\n");
+        else
+            System.out.println("Cycle on edge failed\n");
         
         //Waheed Defensive code tester
         //---------------------------------------------------------------------------------------------
